ee33196d2c98d743cb0b712eea1e6331bb5d8827
Daniel <listda7921@gmail.com>
2020-07-18 11:29:50 -0400

    checkin

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,7 @@ git commit -m"checkin"
 git push origin master
 
 
-rm -Path 'D:\Code\githubPages\githubPagesRelease\docs\*' 
+rm -Path 'D:\Code\githubPages\githubPagesRelease\docs\*' -include *.js, *.css
 cp ../github-page/docs ../githubPagesRelease -r -Force
 cd ../githubPagesRelease/docs
 
